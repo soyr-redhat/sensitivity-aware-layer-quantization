6b93d7c9a21235b8ab12ad2751498b384a2409ac
Sawyer Bowerman <sbowerma@redhat.com>
2026-03-31 20:00:00 -0400

    update missing import

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,11 +1,12 @@
 """Routing pattern analysis utilities."""
 
 from .patterns import analyze_routing_patterns, compute_routing_entropy
-from .visualize import plot_expert_usage, plot_routing_heatmap
+from .visualize import plot_expert_usage, plot_routing_heatmap, plot_entropy_by_layer
 
 __all__ = [
     'analyze_routing_patterns',
     'compute_routing_entropy',
     'plot_expert_usage',
-    'plot_routing_heatmap'
+    'plot_routing_heatmap',
+    'plot_entropy_by_layer'
 ]
